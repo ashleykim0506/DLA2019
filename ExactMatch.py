02e99bca9e2af89badc68f7f725a98292c0db41f
Ashley Kim <ashley@Ashleys-MacBook-Pro.local>
2019-10-08 13:46:40 -0600

    added defensive coding

--- ExactMatch.py
+++ ExactMatch.py
@@ -1,11 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from TIFF_reader import Read_Tiff
+import time
 
 path = 'dataset/'
 
 def getExactMatch(dataDict, targetFile):
-	targetI = plt.imread(path+targetFile, format='grayscale')
+	try:
+		targetI = plt.imread(path+targetFile, format='grayscale')
+	except:
+		print("the name of the file does not exist in the dataset.\n")
+		return -1
 
 	#O(n)
 	for key in dataDict:
@@ -13,12 +18,15 @@ def getExactMatch(dataDict, targetFile):
 			return key
 
 def main():
-	dataDict = Read_Tiff()
+	dataDict, num_file = Read_Tiff()
+	start = time.time()
+
 	targetFile = input("please enter the name of the target file. (ex. 0932.tiff)\n")
 	key = getExactMatch(dataDict, targetFile)
 
+	end = time.time()
 	if(key == targetFile):
-		print('Success! Target file is: ', key)
+		print('Success! Target file (', key, ") found in ", round(end-start,3), "seconds.")
 	else:
 		print('Failed to find target file')
 
